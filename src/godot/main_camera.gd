2ef78573ebb0a74e97b8d1388acdb56606905db7
PatzEdi <edwardferrari717@gmail.com>
2024-06-07 11:58:55 -0700

    Add image saving, image data saving to file. Prepare to add such code to 'get_{image_type}' funcs.
    This commit largelt comprises of test functions that hold the basis for the future implementation of such mechanisms in functions such as the get_{image_type} functions. It is in perparation for the full data gathering process. This commit also prepares for the solving of the look_at problem, explained in the note at the end of the main_camera.gd file.

--- src/godot/main_camera.gd
+++ src/godot/main_camera.gd
@@ -1,21 +1,32 @@
 extends Camera3D
 
 # We use the DockIndicator (Red Dot) as a reference to the dock itself.
-@onready var DockIndicator : MeshInstance3D = get_parent().get_node("meshes/Dock/DockIndicator")
+@onready var DockIndicator : MeshInstance3D = get_parent().get_parent().get_parent().get_node("meshes/Dock/DockIndicator")
 
 # Let's define some variables for the reverse line.
 var reverse_line_distance_from_dock = 2 # distance between the reverse line and the dock.
 
 # Let's define some hyperparameters for the image gathering:
-var save_images = false # Determines whether or not to save the images in the current run.
-var num_images_per_class = 1000 # To ensure that we have an equal number of images for each class, we will set a fixed amount instead of just using random values for camera movements.
+var save_images = true # Determines whether or not to save the images in the current run.
+var num_images_per_class = 50 # To ensure that we have an equal number of images for each class, we will set a fixed amount instead of just using random values for camera movements.
 var num_classes = 3 # There are three classes as of now (a fourth will be added later): center, left, and right
 
 # This counter will count the amount of images we have taken. It will be used to assign image names e.g. image_1.png, image_2.png, etc.
 var global_image_counter = 0
 
+# This below is to determine whether or not the images being gathered will face directly at the reverse line point or not. This will be changed by the code itself using counter variables. Half the images true, half of them false
+var perfectly_centered = false
+
+# This below is to save the image lines to then put in a file.
+var image_data_lines = []
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	await get_tree().process_frame # We wait for the frames to render as an extra precaution to avoid null/black viewport textures.
+	
+	# We set a title for the elements in the data file in csv format:
+	image_data_lines.append("Image, Distance_rline, Random_x, Rotation_value")
+	
 	# Let's print out some information based on the hyperparameters defined above:
 	if (save_images):
 		print("Save images is true, images will be saved")
@@ -23,13 +34,13 @@ func _ready():
 		print("SAVE IMAGES IS FALSE, IMAGES WILL NOT BE SAVED")
 	
 	# Let's center the camera so that it is not too high but also not too low:
-	self.transform.origin.x = DockIndicator.global_position.x + 5 # The "+2" doesn't change anything. Feel free to remove it.
+	self.transform.origin.x = DockIndicator.global_position.x + reverse_line_distance_from_dock # We start at the reverse line. This is important because each function that moves the camera then comes back to this position after taking the picture. original_pos in the functions essentially represent the reverse line.
 	self.transform.origin.y = DockIndicator.global_position.y + .3 # .3 is chosen has the level of the y axis for the camera. It aims it more down toward the reverse line rather than leveled, which would be unrealistic on the robot at hand.
 	self.transform.origin.z = DockIndicator.global_position.z
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	test_positional_movement_x()
 	# Put your code here, to move the camera (e.g functions):
 	get_center_image() # Get's a single image in the center line.
 	
@@ -38,14 +49,19 @@ func _process(_delta):
 	get_right_image() # Get's a single image to the right of the center line.
 	
 	if (global_image_counter == num_images_per_class * num_classes):
+		
+		if (save_images):
+			print("\nWriting image data lines to file...")
+			write_lines_to_file()
 		# Data gathering is finished at this point and it is safe to exit the process.
 		print("\nGathering of data has finished.")
+
 		get_tree().quit() # terminate the data gathering.
 	
 # PARAMS for the functions below:
 # 1. perfectly_centered deterimes whether or not the pictures taken are directly facing the reverse line point (true). If this is toggled off (false), the pictures taken will not be facing directly, and thus there will be a rotation value other than 0.
 
-func get_center_image(perfectly_centered=false):
+func get_center_image():
 	# Put your code here to move the camera:
 	
 	
@@ -58,10 +74,8 @@ func get_center_image(perfectly_centered=false):
 	# Put your code here to save the image:
 	if (save_images):
 		pass
-	
 
-
-func get_left_image(perfectly_centered=false):
+func get_left_image():
 	# Put your code here to move the camera:
 	
 	
@@ -75,7 +89,7 @@ func get_left_image(perfectly_centered=false):
 	if (save_images):
 		pass
 
-func get_right_image(perfectly_centered=false):
+func get_right_image():
 	# Put your code here to move the camera:
 	
 	
@@ -89,9 +103,11 @@ func get_right_image(perfectly_centered=false):
 	if (save_images):
 		pass
 
-
+# This will be done soon as well, it is to get images at the reverse line point:
+func get_reverse_line_point_image():
+	pass
 # This will be configured later. It is used to get the images that have passed the reverse line. This is useful if the robot is placed passed the reverse line manually. It will consist of going backward rather than forward.
-func get_surpassed_images(perfectly_centered=false):
+func get_surpassed_images():
 	# Put your code here:
 	
 	
@@ -104,3 +120,56 @@ func get_surpassed_images(perfectly_centered=false):
 	# Put your code here to save the image:
 	if (save_images):
 		pass
+
+# TEST FUNCTIONS (The below functions are used for quick tests):
+
+func test_rotation_difference(): # Used to see if rotation is retrievable and what values it corresponds to (initial testing purposes):
+	var rotation_value = 0
+	# We look at the reverse line point.
+	self.look_at(Vector3(DockIndicator.global_position.x + reverse_line_distance_from_dock, DockIndicator.global_position.y, DockIndicator.global_position.z))
+	# After that, we add a random rotation to it if perfectly_centered is false. perfectly centered will most likely be a global variable, rater than a parameter of these functions.
+	if (!perfectly_centered):
+		# The random rotation still needs to be thought out. Instead of a random number within a fixed range, we should most likely make such a range adaptive according to the distance from the reverse point and the camera. The farther away, the greater the range, and vice versa. This is because as we get closer, the ability to fit in the entire within a fixed range dock gets smaller. (Verify this? Perhaps we want it to learn to move even when only some of the dock can be seen)
+		# For now, though, we use a fixed range.
+		var degree_range = 30 # This is in plus/minus. In other words it is the max amount that the random rotation can go away from 0, either positive or negative. This IS in degrees.
+		rotation_value = randi_range(-degree_range, degree_range) # The random rotation value, which will also be used as a data point for the image being taken.
+		self.rotation.y += deg_to_rad(rotation_value) # IMPORTANT: We need to convert the degrees generated into radians, as that is how Godot stores the rotation values.
+		# Note: Positive rotation_value = rotate left. Negative rotation value = rotate right.
+	return rotation_value
+
+func test_positional_movement_x():
+	# We store the original position (at the reverse line) in a variable, so that after we do the movement, we can go back to this position:
+	var original_pos = self.transform.origin
+	var random_x = randi_range(1,7) # For now, we do 1-7 in order to avoid look_at debug errors. If 0, it would be directly above.
+	# Put your code here, to move the camera:
+	self.transform.origin.x += random_x
+	# We get the rotation value
+	var rotation_value = test_rotation_difference()
+	
+	await get_tree().process_frame # This is vital, and is needed in order to render the viewport before capturing the image.
+	
+	var distance_from_reverse_line = self.transform.origin.distance_to(original_pos)
+	
+	var img = get_viewport().get_texture().get_image()
+	
+	# Save the image and append to the image_data_lines list.
+	if (save_images):
+		img.save_png("res://data_images/image_" + str(global_image_counter) + ".png")
+		image_data_lines.append("image_" + str(global_image_counter) + ", Distance rline: " + str(distance_from_reverse_line) + ", random_x: " + str(random_x) + ", rotation_value: " + str(rotation_value))
+	
+	# Then, we move back to the original position. We will do this for all three of the get_center, get_left, and get_right funcs.
+	self.transform.origin = original_pos
+
+# Used to write the contents of image_data_lines to a file:
+func write_lines_to_file():
+	var file = FileAccess.open("res://data_text/image_info_data.txt", FileAccess.WRITE)
+	for element in image_data_lines:
+		file.store_line(element)
+	file.close()
+
+
+# NOTE: # Something to be aware of on line 142 (This is related to the refactoring/repositioning of things, said down below at line 173): When this random_x is 0, it means the camera is at the reverse_line point. Since the camera is facing the reverse line point, the picture shows as slanted, but with a rotation value is messed up. To fix this, the rotation value would have to be changed according to the distance value. If the distance value is 0, that is when the camera should use the DockIndicator as a reference point rather than the reverse line point, as that is when the next phase of the autodock starts.
+# Ah, I found the error of the look_at. It was about the random_x. When it would be 0, it would be right on top of the reverse line point, causing the error.
+
+# So what is the next thing to do? Restructure the code to use the code in theese test functions, but make it so that it differentiates between when the camera is directly on the reverse line point and when it is above.
+# Essentially just figure out how everything should be structured in terms of ensuring that the data will be equal for all of the image classes.
