86342b8383df3a7e96147848b64586a815d1e21e
PatzEdi <edwardferrari717@gmail.com>
2024-06-10 16:32:47 -0700

    Fix a test function & add Godot data folders.
    I had forgotten to include the data directories for the data gathering under the godot folder. Apologies :0

--- tests/tests.py
+++ tests/tests.py
@@ -3,13 +3,6 @@ import polars as pl
 import os
 import sys
 
-# Add the path where data_process.py is located to the system path
-current_script_path = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.join(current_script_path, '../src/machine_learning/'))
-
-# Now you can import your module
-#import data_process
-
 class TestDataProcess(unittest.TestCase):
     def test_data_size(self):
         # Define a path to a test CSV file
@@ -17,7 +10,7 @@ class TestDataProcess(unittest.TestCase):
 
 
         # Read the CSV file using the function you're testing
-        df = pl.read_csv(test_csv_path)
+        df = pl.read_csv(test_csv_path, infer_schema_length=10000)
 
         # Assert that the DataFrame has the correct shape
         self.assertEqual(df.shape, (1800, 5))
