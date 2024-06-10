c94fca7abcadd822d09d5443a439d86b5d43641e
PatzEdi <edwardferrari717@gmail.com>
2024-06-09 18:56:57 -0700

    Added data loading and processing in data_process.py in preparation for machine learning phase.
    Machine learning phase starts after this commit (if there are no obstacles :))

--- src/machine_learning/data_process.py
+++ src/machine_learning/data_process.py
@@ -1,10 +1,13 @@
 # This script will be used to gather the necessary data for training the model
-
+print("\nImporting libraries...")
 # General system libraries:
 import os
 # Data libraries:
 import polars as pl
 import csv
+import torch
+
+print("\nDone importing libraries.")
 
 # First, let's define the paths of the data, using os so that we can use relative paths:
 current_script_path = os.path.dirname(os.path.abspath(__file__))
@@ -12,7 +15,33 @@ current_script_path = os.path.dirname(os.path.abspath(__file__))
 data_images_path = os.path.join(current_script_path, '../godot/data_images')
 data_image_text_path = os.path.join(current_script_path, '../godot/data_text/image_info_data.txt')
 
-# Let's read the data from a csv file using polars (very fast)
-df = pl.read_csv(data_image_text_path)
+print("\nLoading data from: " + data_image_text_path + "using polars...")
+# Let's read and load the data from a csv file using polars (very fast)
+df = pl.read_csv(data_image_text_path) # We technically have all of the data here to train the model.
 # Let's print out the contents:
 print(df)
+print()
+# Let's print out the amount of images that are in the data, which is the amount of rows:
+print("Amount of images: " + str(df.shape[0]))
+
+image_paths = df['Image'].to_list()
+
+directions = df['Direction'].to_list()
+distances = df['Distance_rline'].to_list()
+rotation_values = df['Rotation_value'].to_list()
+
+data_targets = []
+
+# Let's get the data target values in a list of lists, with each inner list containing the values of the target values for each image. The 'Type' column we can skip, so we start at everything after that.
+for i in range(df.shape[0]):
+    data_targets.append([directions[i],distances[i],rotation_values[i]])
+
+def get_data_targets(convert_targets_to_tensor=False):
+    if (convert_targets_to_tensor):
+        for i in range(len(data_targets)):
+            for i2 in range(len(data_targets[i])):
+                data_targets[i][i2] = torch.tensor(data_targets[i][i2], dtype=torch.int64) if i2 == 0 else torch.tensor(data_targets[i][i2], dtype=torch.float64)
+    return data_targets
+
+data_targets = get_data_targets(True)
+print(data_targets[1])
